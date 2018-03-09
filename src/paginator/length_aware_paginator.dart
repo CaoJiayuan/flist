9b7a0afc06815256a52ed690b2346a994421ab86
cjy <cjy632258@hotmail.com>
2018-03-09 11:56:07 +0800

    update

--- src/paginator/length_aware_paginator.dart
+++ src/paginator/length_aware_paginator.dart
@@ -7,21 +7,25 @@ import 'package:darequest/request.dart';
 import 'paginator.dart';
 
 class LengthAwarePaginator extends Paginator {
+  String url;
 
   int total = 0;
   int perPage = 15;
-  int currentPage = 1;
-  int lastPage = 1;
+  int lastPage = 0;
   String nextPageUrl;
   String prevPageUrl;
   int from = 0;
   int to = 0;
   List<Map<String, Object>> data = [];
 
+  LengthAwarePaginator({this.url});
+
+  bool get hasNextPage => this.nextPageUrl != null;
+
   @override
   Future<List<Map<String, Object>>> onLoad(Response<HttpClientResponse> response) async {
-    var responseBody ;
-    if(Request.parseResponse) {
+    var responseBody;
+    if (Request.parseResponse) {
       responseBody = response.responseBody;
     } else {
       var body = await response.response.transform(UTF8.decoder).join();
@@ -38,4 +42,24 @@ class LengthAwarePaginator extends Paginator {
     data = responseBody['data'];
     return data;
   }
+
+  @override
+  String getUrl() {
+    return this.url;
+  }
+
+  Map<String, Object> toMap() {
+    Map<String, Object> map = new Map<String, Object>();
+    map['total'] = total;
+    map['per_page'] = perPage;
+    map['current_page'] = currentPage;
+    map['last_page'] = lastPage;
+    map['next_page_url'] = nextPageUrl;
+    map['prev_page_url'] = prevPageUrl;
+    map['from'] = from;
+    map['to'] = to;
+    map['data'] = data;
+    return map;
+  }
+
 }
--- src/paginator/paginator.dart
+++ src/paginator/paginator.dart
@@ -4,15 +4,42 @@ import 'dart:io';
 import 'package:darequest/request.dart';
 
 abstract class Paginator {
-  final String url;
 
-  Paginator({this.url});
+  int currentPage = 0;
 
-  Future<List> load([Map<String, dynamic> params = const {}]) {
-    return Request.get(url, {"params": params}).then((response) {
+
+  Future<List> load([Map<String, dynamic> params]) {
+    return Request.get(getUrl(), {"params": params}).then((response) {
      return onLoad(response);
     });
   }
 
+  Future<List> page(int page, [Map<String, dynamic> params]){
+    if (params == null) {
+      params = {};
+    }
+    params['page'] = page;
+    return load(params);
+  }
+
+  Future<List> next([Map<String, dynamic> params]){
+    if (params == null) {
+      params = {};
+    }
+    params['page'] = currentPage + 1;
+    return load(params);
+  }
+
+  Future<List> prev([Map<String, dynamic> params]){
+    if (params == null) {
+      params = {};
+    }
+    params['page'] = currentPage - 1;
+    return load(params);
+  }
+
+
   Future<List> onLoad(Response<HttpClientResponse> response);
+
+  String getUrl();
 }
